de5f4a99b211fe96af5c73a2c509b4e4665217ec
Ruwanya Abeynayake <ruwanya.abeynayake@thakshana.com>
2026-04-11 15:03:09 +0530

    added the console calcaultor

---
+++ CPP_language/CPP_projects/11.switch/d.console_calcualtor_program.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main()
+{
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "****** CALCULATOR ******" << std::endl;
+
+    std::cout << "Enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            std::cout << "Invalid operator" << std::endl;
+            break;
+    }
+    std::cout << "************************" << std::endl;
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
